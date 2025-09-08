5cb7964abe01d5437178c7dc89fc24eb3e2a3288
handabao <18862963156@163.com>
2025-09-08 10:28:13 +0800

    update

--- src/ReadProcessMemory/src/main.cpp
+++ src/ReadProcessMemory/src/main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     // HWND h = FindWindowA("EVERYTHING", "Everything");
-    HWND h = FindWindowA(NULL, "计算器");
+    // HWND h = FindWindowA(NULL, "计算器");
+    HWND h = FindWindowA("MainWindow", "植物大战僵尸中文版");
     printf("窗口句柄 h=%p\r\n", h);
 
     DWORD pid, tid;
@@ -20,12 +21,12 @@ int main(int argc, char *argv[])
     HANDLE 进程句柄 = OpenProcess(PROCESS_ALL_ACCESS, 0, pid); /// HWND
     printf("进程句柄=%p \r\n", 进程句柄);
     unsigned int 返回值 = 0;
-    ReadProcessMemory(进程句柄, (LPCVOID)0x1841D2BA400 /*要读取的地址*/, &返回值 /*存放数据的地址*/, 4, 0);
+    ReadProcessMemory(进程句柄, (LPCVOID)0x14B9D230 /*要读取的地址*/, &返回值 /*存放数据的地址*/, 4, 0);
     printf("返回值=%X  返回值=%d 返回值=%u\r\n", 返回值, 返回值, 返回值); ///  GetLastError();
 
     /// 要写入的新值
-    unsigned int 新值 = 42; /// 替换为您要写入的值
-    if (!WriteProcessMemory(进程句柄, (LPVOID)0x1841D2BA400, &新值, sizeof(新值), nullptr))
+    unsigned int 新值 = 1000; /// 替换为您要写入的值
+    if (!WriteProcessMemory(进程句柄, (LPVOID)0x14B9D230, &新值, sizeof(新值), nullptr))
     {
         printf("写入内存失败，错误代码=%lu\r\n", GetLastError());
     }
