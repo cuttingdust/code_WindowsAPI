351cfa4e5ff1d7def1147766ed6f362300d38a9b
handabao <18862963156@163.com>
2025-10-28 14:54:47 +0800

    update

--- src/CreateProcess/src/main.cpp
+++ src/CreateProcess/src/main.cpp
@@ -77,14 +77,14 @@ int main(int argc, char *argv[])
         // si.dwFillAttribute = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | BACKGROUND_BLUE;
         /// 这个没用 因为是创建进程是GUI， 所以没有颜色控制台
         si.wShowWindow = TRUE;
-        BOOL bRet      = ::CreateProcess(NULL,               /// 应用程序名（使用命令行）
-                                         szCommandLine,      /// 命令行字符串
-                                         NULL,               /// 进程安全属性
-                                         NULL,               /// 线程安全属性
-                                         FALSE,              /// 句柄继承选项
-                                         CREATE_NEW_CONSOLE, /// 创建标志
-                                         NULL,               /// 环境块
-                                         NULL,               /// 当前目录
+        BOOL bRet      = ::CreateProcess(NULL,               /// 不在此指定可执行文件的文件名
+                                         szCommandLine,      /// 命令行参数
+                                         NULL,               /// 默认进程安全性
+                                         NULL,               /// 默认线程安全性
+                                         FALSE,              /// 指定当前进程内的句柄不可以被子进程继承
+                                         CREATE_NEW_CONSOLE, /// 为新进程创建一个新的控制台窗口
+                                         NULL,               /// 使用本进程的环境变量
+                                         NULL,               /// 使用本进程的驱动器和目录
                                          &si,                /// STARTUPINFO  /// （引用计数=2）
                                          &pi                 /// PROCESS_INFORMATION
              );
