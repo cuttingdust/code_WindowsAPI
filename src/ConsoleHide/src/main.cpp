ba527c950b4a2d6a9da30b56f00bd76adc2a15db
handabao <18862963156@163.com>
2025-10-30 15:48:56 +0800

    2-15、通过预处理指令和替代函数实现控制台的显示和隐藏

---
+++ src/ConsoleHide/src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+#include <Windows.h>
+#include <tchar.h>
+
+/// 控制台程序 隐藏控制指令
+/// /entry:\"mainCRTStartup\"
+#pragma comment(linker, "/subsystem:\"windows\" ")
+
+
+int main(int argc, char *argv[])
+{
+    MessageBox(0, TEXT("Console"), TEXT(""), 0);
+    printf("In Console Hide!");
+    getchar();
+    return 0;
+}
+
+int WINAPI WinMain(                       /// 入口函数
+        _In_ HINSTANCE     hInstance,     /// 当前实例句柄
+        _In_opt_ HINSTANCE hPrevInstance, /// 前一个实例句柄（已废弃)
+        _In_ LPSTR         lpCmdLine,     /// 命令行参数字符串
+        _In_ int           nCmdShow       ///  窗口显示方式
+)
+{
+    /// 使用示例：加载应用程序图标
+    // HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPICON));
+
+    return main(__argc, __argv);
+}
