c68c82fb2153d2a318e4c70145d2fefbb78198e3
handabao <18862963156@163.com>
2025-11-28 10:06:52 +0800

    windows 手动 导入库

---
+++ src/LoadLibrary/src/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+/// 正确定义MessageBoxW的函数指针类型
+// typedef int(WINAPI* MESSAGEBOXW)(HWND, LPCWSTR, LPCWSTR, UINT);
+
+int main(void)
+{
+    HMODULE hModule = ::LoadLibrary(L"LoadLibraryTest.dll");
+    // HMODULE hModule = ::LoadLibrary(L"USER32.dll");
+    if (hModule != NULL)
+    {
+        /// 获取函数指针前需要检查是否获取成功
+        // MESSAGEBOXW func = (MESSAGEBOXW)::GetProcAddress(hModule, "MessageBoxW");
+        FARPROC func = (FARPROC)::GetProcAddress(hModule, "?go");
+        if (func != NULL)
+        {
+            // func(NULL, L"测试导入", L"标题", NULL); /// 调用函数
+        }
+        else
+        {
+            printf("在DLL中找不到 'go' 函数\n");
+            DWORD error = GetLastError();
+            printf("错误代码: %d\n", error);
+        }
+
+        /// 卸载DLL
+        FreeLibrary(hModule);
+    }
+    else
+    {
+        printf("没有找到dll\n");
+        DWORD error = GetLastError();
+        printf("错误代码: %d\n", error);
+
+        /// 常见错误代码含义
+        switch (error)
+        {
+            case 2:
+                printf("文件不存在\n");
+                break;
+            case 126:
+                printf("找不到指定的模块\n");
+                break;
+            case 193:
+                printf("不是有效的Win32应用程序\n");
+                break;
+            default:
+                printf("未知错误\n");
+                break;
+        }
+    }
+
+    getchar();
+    return 0;
+}
---
+++ src/LoadLibraryTest/src/test.cpp
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+///如果需要生成可供外部程序调用的函数，必须用dllexport
+_declspec(dllexport) void go()
+{
+    MessageBox(NULL, L"我是来自dll", L"hello Dll", MB_OK);
+}
